44598794b740c6eb9ff9ce1767c9d4fadba0c929
nshaughn <nathanpaulshaughnessy@hotmail.co.uk>
2022-08-01 09:04:05 +0100

    pictures

--- scirpt.js
+++ scirpt.js
@@ -11,27 +11,27 @@ roll.addEventListener('click', () => {
     let number = Math.ceil(Math.random() * 6);
 
     if (number === 1 || score <= 20) {
-        diceFace.src = "/diceGame/Dice/dice-1.png";
+        diceFace.src = "/Week 3/diceGame/Dice/dice-1.png";
         score.textContent = `Loser, you rolled a '1', click restart to play again`;
         document.getElementById('roll').style.visibility='hidden';
     } else if (number == 2) {
-        diceFace.src = "/diceGame/Dice/dice-2.png";
+        diceFace.src = "/Week 3/diceGame/Dice/dice-2.png";
         rolled += 2;
         score.textContent = `${rolled}`;
     } else if (number == 3) {
-        diceFace.src = "/diceGame/Dice/dice-3.png";
+        diceFace.src = "/Week 3/diceGame/Dice/dice-3.png";
         rolled += 3;
         score.textContent = `${rolled}`;
     } else if (number == 4) {
-        diceFace.src = "/diceGame/Dice/dice-4.png";
+        diceFace.src = "/Week 3/diceGame/Dice/dice-4.png";
         rolled += 4;
         score.textContent = `${rolled}`;
     } else if (number == 5) {
-        diceFace.src = "/diceGame/Dice/dice-5.png";
+        diceFace.src = "/Week 3/diceGame/Dice/dice-5.png";
         rolled += 5;
         score.textContent = `${rolled}`;
     } else if (number == 6) {
-        diceFace.src = "/diceGame/Dice/dice-6.png";
+        diceFace.src = "/Week 3/diceGame/Dice/dice-6.png";
         rolled += 6;
         score.textContent = `${rolled}`;
     }
@@ -43,7 +43,7 @@ roll.addEventListener('click', () => {
 
 restart.addEventListener('click', () => {
     rolled = 0;
-    diceFace.src = "/diceGame/Dice/dice-1.png";
+    diceFace.src = "/Week 3/diceGame/Dice/dice-1.png";
     score.textContent = `0`;
     document.getElementById('roll').style.visibility='visible';
 
